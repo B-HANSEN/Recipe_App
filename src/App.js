42e3cd8689b488014ec253a8293f5855fd4c6153
B-HANSEN <b.hansen.ubiqum@gmail.com>
2019-12-11 22:10:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = '5ed30c36';
+  const APP_KEY = '327d7c23d17804324991002298b79eef';
+  const exampleReq =
+   `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <form className="search-form">
+       <input className="search-bar" type="text" />
+      <button className="search-button" type="submit">
+        Search
+      </button>
+     </form>
     </div>
-  );
+  )
 }
+
+
+
+
+
 
 export default App;
